fc44330f1198610a52fdf8ed38cd4bd23fc86bb3
rej <rej.safranko@codxsolutions.hr>
2024-06-26 12:24:15 +0200

    finished authentication and project initialization

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="mlcontrol",
     version="0.1",
-    py_modules=["mlcontrol"],
+    packages=find_packages(),
     install_requires=[
-        "click",
-        "google-api-python-client",
-        "google-auth",
-        "google-auth-oauthlib",
-        "google-auth-httplib2",
+        "click==8.1.7",
+        "google-api-python-client==2.134.0",
+        "google-auth==2.30.0",
+        "google-auth-oauthlib==1.2.0",
+        "google-auth-httplib2==0.2.0",
     ],
-    entry_points="""
-        [console_scripts]
-        mlcontrol=mlcontrol:cli
-    """,
+    entry_points={
+        "console_scripts": [
+            "mlcontrol=mlcontrol.cli:mlcontrol",
+        ],
+    },
 )
