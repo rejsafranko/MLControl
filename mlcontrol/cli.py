a61c660becef03ee3cdbb43142c074d115c0be9c
rej <rej.safranko@codxsolutions.hr>
2024-07-11 13:03:15 +0200

    added help to gpus command

--- mlcontrol/cli.py
+++ mlcontrol/cli.py
@@ -98,9 +98,21 @@ def list(project_name: str, folder_type: str) -> None:
 
 
 @click.command()
-def gpus():
+@click.option("--help", "help", flag_value=True)
+def gpus(help) -> None:
     """Lists available GPUs provided by Vast.ai cloud service."""
     command = "vastai search offers"
+    if help:
+        click.echo(
+            """
+disk_space: float       disk storage space, in GB
+gpu_arch: string        host machine gpu architecture (e.g. nvidia, amd)
+gpu_ram: float          per GPU RAM in GB
+rentable: bool          is the instance currently rentable
+verified: bool          is the machine verified
+            """
+        )
+        return
     try:
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
